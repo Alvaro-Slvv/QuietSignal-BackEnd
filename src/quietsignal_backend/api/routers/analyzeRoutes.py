9d3d898831139d13d752c4b784b711fbc6dc76d4
Alvaro Silva <102283382+BabyScylla69@users.noreply.github.com>
2025-12-03 13:22:29 +0100

    added api response logic

--- src/quietsignal_backend/api/routers/analyzeRoutes.py
+++ src/quietsignal_backend/api/routers/analyzeRoutes.py
@@ -1,12 +1,31 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+
 from ...database import get_db
 from ...services.analyzeService import AnalyzeService
 from ...models.dto.analyzeDTO import AnalyzeRequestDTO, AnalyzeResponseDTO
 
-router = APIRouter(prefix="/analyze", tags=["analyze"])
+router = APIRouter(prefix="/analyze", tags=["Analyze"])
 service = AnalyzeService()
 
-@router.post("/", response_model=AnalyzeResponseDTO)
+
+@router.post("/", response_model=dict)
 async def analyze(request: AnalyzeRequestDTO, db: Session = Depends(get_db)):
-    return service.analyze(request)
+    try:
+        result = service.analyze(request, db)
+
+        # result is expected: { "label": str, "probabilities": dict }
+        return {
+            "success": True,
+            "status_code": 200,
+            "message": "Analysis completed successfully",
+            "data": result,
+        }
+
+    except Exception as e:
+        return {
+            "success": False,
+            "status_code": 500,
+            "message": "Analysis failed",
+            "errors": str(e),
+        }
--- src/quietsignal_backend/api/routers/userRoutes.py
+++ src/quietsignal_backend/api/routers/userRoutes.py
@@ -6,14 +6,33 @@ from ...services.userService import UserService
 from ...database import get_db
 from ...api.deps import get_current_user
 
-router = APIRouter(prefix="/users", tags=["users"])
+router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.post("/", response_model=UserOutDTO)
+@router.post("/")
 def create_user(user_in: UserCreateDTO, db: Session = Depends(get_db)):
-    return UserService.create_user(db, user_in)
+    try:
+        user = UserService.create_user(db, user_in)
+        return {
+            "success": True,
+            "status_code": 201,
+            "message": "User created",
+            "data": UserOutDTO.model_validate(user),
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "status_code": 500,
+            "message": "User creation failed",
+            "errors": str(e),
+        }
 
 
-@router.get("/me", response_model=UserOutDTO)
-def me(user=Depends(get_current_user)): # if admin admin_user = Depends(require_role("admin"))
-    return UserOutDTO.model_validate(user)
+@router.get("/me")
+def me(user=Depends(get_current_user)):
+    return {
+        "success": True,
+        "status_code": 200,
+        "message": "Fetched user",
+        "data": UserOutDTO.model_validate(user),
+    }
